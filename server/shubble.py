4ec45905533d5ac785fa3c24bc37c6ef9917f963
Joel McCandless <mail@joelmccandless.com>
2025-04-11 22:35:09 -0400

    serve react through Flask server

--- server/shubble.py
+++ server/shubble.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from pathlib import Path
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build/', static_url_path='/')
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<path:path>")
+def serve_react(path=""):
+    root_dir = Path(app.static_folder)
+    file_path = root_dir / path
+
+    if file_path.exists() and file_path.is_file():
+        return send_from_directory(root_dir, path)
+    else:
+        return send_from_directory(root_dir, "index.html")
+
