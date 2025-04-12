ecf8c0918f3f3825aec39fc617ef36d61abd5486
Joel McCandless <mail@joelmccandless.com>
2025-04-11 21:22:46 -0400

    create header

--- app/README.md
+++ app/README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Color Palette
+
+View the latest color palette [here](https://coolors.co/a50104-f3f3f3-141301-274c77-6096ba).
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {
+    useState,
+} from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
+import { LuMenu } from "react-icons/lu";
 
 function Header() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Live Location</Link>
-                </li>
-                <li>
-                    <Link to="/schedule">Schedule</Link>
-                </li>
-            </ul>
-        </nav>
+        <header className='header'>
+            <div className='header-top'>
+                <div className='title'>
+                    <h1>
+                        SHUBBLE
+                    </h1>
+                </div>
+                <button className='menu-button' onClick={toggleMenu}>
+                    <LuMenu className='menu-icon' />
+                </button>
+            </div>
+            { isOpen && (
+                <nav className='nav'>
+                    <ul>
+                        <li>
+                            <Link to="/">Live Location</Link>
+                        </li>
+                        <li>
+                            <Link to="/schedule">Schedule</Link>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </header>
     );
 }
 
