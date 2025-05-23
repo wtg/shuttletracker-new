616e01fbce7ff10190a2f0e32b062af45dc13911
Joel McCandless <mail@joelmccandless.com>
2025-05-22 21:15:53 -0400

    change coords in routes

--- client/src/pages/MapKitMap.jsx
+++ client/src/pages/MapKitMap.jsx
@@ -294,11 +294,11 @@ export default function MapKitMap({ vehicles }) {
                 destination: new window.mapkit.Coordinate(42.73697970529986, -73.6667971588316),
             },
             {
-                origin: new window.mapkit.Coordinate(42.73697970529986, -73.6667971588316),/*
+                origin: new window.mapkit.Coordinate(42.73697970529986, -73.6667971588316),
                 destination: new window.mapkit.Coordinate(42.736332119185015, -73.66693506284926),
             },
             {
-                origin: new window.mapkit.Coordinate(42.736332119185015, -73.66693506284926),
+                origin: new window.mapkit.Coordinate(42.736332119185015, -73.66693506284926),/*
                 destination: new window.mapkit.Coordinate(42.735805363425634, -73.66618733238738),
             },
             {
